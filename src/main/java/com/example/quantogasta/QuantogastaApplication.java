b8dcbe9cafc3c82c2b8c76154e8aa213792072ac
YuriAlves25 <yuri_alves00@hotmail.com>
2024-03-18 15:07:39 -0300

    First commit

---
+++ src/main/java/com/example/quantogasta/QuantogastaApplication.java
@@ -0,0 +1,13 @@
+package com.example.quantogasta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantogastaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantogastaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quantogasta/QuantogastaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quantogasta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantogastaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
