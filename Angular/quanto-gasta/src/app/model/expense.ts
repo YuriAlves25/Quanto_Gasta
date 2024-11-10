708210173ba9b1c0f8da4fc0432ad151fd27f19d
YuriAlves25 <134546397+YuriAlves25@users.noreply.github.com>
2024-11-10 17:10:16 -0300

    Delete Angular directory

--- Angular/quanto-gasta/src/app/model/expense.ts
+++
@@ -1,6 +0,0 @@
-export interface Expense {
-  id: string;
-  money: number;
-  category: string;
-  yearMonth: string;
-}
--- Angular/quanto-gasta/src/app/model/month-expenses.ts
+++
@@ -1,8 +0,0 @@
-import { Expense } from "./expense";
-
-export interface MonthExpenses {
-  date: string;
-  expenses: Expense[];
-  id: string;
-  total: number;
-}
